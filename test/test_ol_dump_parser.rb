4ecb26b25a4f4afe5668eb96818529b07d53c7c8
arjun <arjundas.27586@gmail.com>
2021-04-03 21:12:25 +0530

    Updated tests

--- test/test_ol_dump_parser.rb
+++ test/test_ol_dump_parser.rb
@@ -4,15 +4,20 @@ require 'byebug'
 
 class OldDumpParserTest < Minitest::Test
   def setup
-    out_dir_path = './output'
-    FileUtils.mkdir_p out_dir_path
+    @out_dir_path = './output'
+    FileUtils.mkdir_p @out_dir_path
     @ol_dump_parser = OlDumpParser.new({ out_file: './output/out.json' })
-    @test_files_path = File.expand_path('./test_files', File.dirname(__FILE__))
+    @test_files_inp_path = File.expand_path('./test_files/inp', File.dirname(__FILE__))
+    @test_files_out_path = File.expand_path('./test_files/out', File.dirname(__FILE__))
   end
-
-  def test_english_hello
-    @ol_dump_parser.inp_file = @test_files_path + '/authors_5_lines.txt'
+  
+  def teardown
+    FileUtils.remove_dir @out_dir_path, true
+  end
+  
+  def test_parsing_authors_dump
+    @ol_dump_parser.inp_file = @test_files_inp_path + '/authors_5_lines.txt'
     @ol_dump_parser.parse
-    
+    assert_equal File.read(@test_files_out_path + '/authors_5_lines.json'), File.read(@ol_dump_parser.out_file)
   end
 end
